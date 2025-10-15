1989035e1a0b3019319e1daef8e6e0e24ee4402d
Dinesh-Kumar-Tunguturi <dineshkumartunguturi@gamil.com>
2025-10-15 12:52:32 +0530

    fix: set DJANGO_SETTINGS_MODULE in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,7 @@
-﻿from django.core.wsgi import get_wsgi_application
+﻿import os
+from django.core.wsgi import get_wsgi_application
+
+# ✅ Must set the Django settings module before anything else
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "profile_scoring.settings")
 
 app = get_wsgi_application()
